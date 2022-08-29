8e163f7dab1e70a919898780f5eb1576df5f444a
Renat <fricentaurus@GMAIL.COM>
2022-08-29 13:34:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import MouseTracker from "./MouseTracker";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <MouseTracker />
+    </div>
+  );
+}
